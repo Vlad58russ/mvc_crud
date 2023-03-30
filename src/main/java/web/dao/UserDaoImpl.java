36f5e1b54b187e9622b6d3c61f912317daf47cce
Vlad <vladschenn@yandex.ru>
2023-03-30 19:48:05 +0300

    Initial commit

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,39 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return entityManager.createQuery("FROM User", User.class).getResultList();
+    }
+
+    @Override
+    public User getById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+}
---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,17 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void addUser(User user);
+
+    void deleteUser(Long id);
+
+    void updateUser(User user);
+
+    List<User> getAllUser();
+
+    User getById(Long id);
+}
